671c58b96f23b516696f9b1935e488028c626e11
kerneq <stasik-0@yandex.ru>
2017-03-14 19:42:09 +0300

    add dataSets module

---
+++ src/main/java/db/dataSets/ParsedGroup.java
@@ -0,0 +1,16 @@
+package db.dataSets;
+
+/**
+ * Created by iters on 3/14/17.
+ */
+public class ParsedGroup {
+    public int id;
+    public int pubId;
+    public int peopleCount;
+
+    public ParsedGroup(int id, int pubId, int peopleCount) {
+        this.id = id;
+        this.pubId = pubId;
+        this.peopleCount = peopleCount;
+    }
+}
